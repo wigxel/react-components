15db63e095996fbffe707ab680a289cc20d18176
Joseph Owonvwon <Joseph.owonwo@gmail.com>
2020-08-22 03:36:57 +0100

    :sparkle: Added morph prop to Collapisble Component.

--- src/components/lists/Collapsible.js
+++ src/components/lists/Collapsible.js
@@ -2,9 +2,6 @@ import React, { useRef, useEffect, useState } from "react"
 import t from "prop-types"
 import styled from "styled-components"
 
-// TODO: complete the collapsibla component
-//add an morph property
-
 const CollapseStyle = styled.div`
   height: 0;
   padding: 0 0;
@@ -20,10 +17,29 @@ const CollapseStyle = styled.div`
   }
 `
 
-export const Collapsible = props => {
+const makeObserver = (onChange) =>
+	new MutationObserver(function (mutationsList, observer) {
+		for (let mutation of mutationsList) {
+			console.log("Mutation", mutation)
+			onChange(mutation.type)
+		}
+	})
+
+export const Collapsible = (props) => {
 	const container = useRef()
 	const [height, setHeight] = useState(0)
-	const getHeight = el => el.getBoundingClientRect().height
+	const getHeight = (el) => el.getBoundingClientRect().height
+
+	useEffect(() => {
+		const observer = makeObserver(() =>
+			setHeight(getHeight(container.current))
+		)
+		if (props.morph === true) {
+			observer.observe(container.current, { childList: true, subtree: true })
+		}
+
+		return () => observer.disconnect()
+	}, [props.morph])
 
 	useEffect(() => {
 		setHeight(props.open ? getHeight(container.current) : 0)
@@ -37,11 +53,12 @@ export const Collapsible = props => {
 }
 
 Collapsible.defaultProps = {
-	morph: false
+	morph: false,
 }
+
 Collapsible.propTypes = {
 	open: t.bool,
-	children: t.node.isRequired
+	children: t.node.isRequired,
 }
 
 export default Collapsible
